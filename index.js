3cdc90988a0b953c0ca9286b4818bada360ecd95
Stefano Miceli <mic.ninja@gmail.com>
2024-03-12 12:32:19 +0100

    adds manipulation examples

--- index.js
+++ index.js
@@ -69,8 +69,10 @@ console.log(loneWolfSpan);
 const mainTitleWithQS = document.querySelector(".main-title");
 console.log(mainTitleWithQS);
 
-const menuWithQS = document.querySelector("#menu li:nth-child(3)"); // utilizzo di qualsiasi selettore CSS avanzato in combinazione col querySelector
+const menuWithQS = document.querySelector("#menu"); // utilizzo di qualsiasi selettore CSS avanzato in combinazione col querySelector
 console.log(menuWithQS);
+const liInMmenuWithQS = document.querySelector("#menu li:nth-child(3)"); // utilizzo di qualsiasi selettore CSS avanzato in combinazione col querySelector
+console.log(liInMmenuWithQS);
 // 5) .querySelectorAll(stringa di selettore CSS)
 // BISOGNA RICORDARSI dei simboli di selezione di CSS (. e #)
 // il query selector torna una NodeList che dovremo eventualmente ciclare (è presente il forEach, ma NON map..filter..ecc)
@@ -81,3 +83,56 @@ console.log(allLisWithQS);
 const allBodyTextWithQS = document.querySelectorAll(".body-text");
 console.log(allBodyTextWithQS);
 allBodyTextWithQS.forEach(p => console.log(p)); // con la NodeList possiamo usare il forEach
+
+// partire da un elemento per selezionare dei figli più interni (senza partire dal document)
+// questo è un metodo per scendere ulteriormente nella ricerca di nodi più interni, a partire da un elemento già trovato
+const firstMainLi = main.querySelector("li");
+console.log(firstMainLi);
+
+// METODI PER RISALIRE L'ALBERO DI NODI
+
+// .parentNode (è la proprietà associata al nodo, NON E' un metodo!)
+console.dir(menuWithQS);
+console.log(menuWithQS.parentNode.parentNode); // risaliamo di due livelli fino ad arrivare al tag <header>
+// risaliamo di due livelli, arrivando al tag main, per poi scendere a selezionare l'h1
+// tre metodi validi per risalire e poi scendere di un livello
+console.dir(firstMainLi.parentNode.parentNode.firstElementChild);
+console.dir(firstMainLi.parentNode.parentNode.querySelector("h1"));
+console.dir(firstMainLi.parentNode.parentNode.children[0]);
+
+// .closest(css selector) - è un metodo che si applica a qualsiasi nodo del DOM e chiede un selettore CSS per
+// cercare verso l'alto il primo match con quel selettore
+console.log(liInMmenuWithQS.closest(".nav-menu"));
+console.log(liInMmenuWithQS.closest("header")); // è come scrivere liInMmenuWithQS.parentNode.parentNode.parentNode
+
+// ___________ MANIPOLAZIONE DEGLI ELEMENTI ____________________________________________________________________
+
+// modifica di stile attraverso la proprietà style esistente su un qualsiasi nodo del DOM
+// loneWolf.style.backgroundColor = "palegreen";
+loneWolf.style = "background-color: palegreen; font-size: 20px; letter-spacing: 2px";
+
+// modifica del contenuto, attraverso innerText (per solo testo) e innerHTML (per valutare anche del codice html)
+loneWolf.innerText = "TI HO CAMBIATO AHAHAHAH";
+loneWolf.innerHTML = `Lorem ipsum, dolor sit <span style="color: red">amet consectetur adipisicing elit</span>. Tenetur vitae, enim beatae cumque
+tempora commodi
+natus alias dignissimos quaerat officiis! Asperiores,<strong> ab saepe nihil</strong> quisquam odit repellat aspernatur
+veniam laudantium!
+Dignissimos, beatae iure!`;
+
+// applicazione di stili
+const toBeRemoved = document.getElementById("to-be-removed");
+console.dir(toBeRemoved);
+
+// toBeRemoved.className = "temporary-class"; // questo metodo SOVRASCRIVE L'INTERA CLASSE dell'elemento
+toBeRemoved.classList.add("temporary-class"); // con questo metodo riusciamo ad aggiungere una classe senza sovrascrivere le altre già presenti
+toBeRemoved.classList.remove("another-class"); // con questo metodo riusciamo ad aggiungere una classe senza sovrascrivere le altre già presenti
+
+// cambiamo testo dopo 1 secondo
+setTimeout(() => {
+  toBeRemoved.innerText = " me ne sto per andare...";
+}, 1000);
+
+// dopo un altro secondo l'elemento scomparirà per via dell'applicazione della classe hidden creata in precedenza nel nostro CSS
+setTimeout(() => {
+  toBeRemoved.classList.add("hidden");
+}, 2000);
